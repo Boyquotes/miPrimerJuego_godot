60f74d702227724fb8e5cb78be2c94d11c91b75d
daniel <110266536+contexto01@users.noreply.github.com>
2022-11-04 19:27:21 -0500

    Create muerte.gd

---
+++ muerte.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+var musica_fondo = preload("res://sonido/loser.ogg")
+
+func _on_Button_pressed():
+	Globalf.vidas = 3
+	Globalf.manzana = 0
+	get_tree().change_scene("res://niveles/nivel1.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://niveles/pantalla_inicio.tscn")
+	pass # Replace with function body.
+
+
+
+func _ready():
+	MusicaDeFondo.musicafondo(musica_fondo)
